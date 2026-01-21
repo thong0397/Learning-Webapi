1b3423feef49cc0ce234b5b4ac532f4892ea3a7a
Nguyen Huu Thong <nguyenhuu.thong@smdautomation.com>
2026-01-21 12:51:50 +0700

    In####itial ASP.NET Core Web API project with CRUD endpoints
    Set up MyWebApi project targeting .NET 8.0. Added HangHoaController with in-memory CRUD operations and HangHoa model. Included WeatherForecast sample API, Swagger support, launch settings, and basic app configuration. Added HTTP request file for endpoint testing.
    
    Initial ASP.NET Core Web API project setup
    
    Added MyWebApi project with controllers for WeatherForecast and HangHoa (CRUD). Included model definitions, configuration files, Swagger support, and sample HTTP requests for API testing.

---
+++ MyWebApi/MyWebApi/Controllers/HangHoaController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApi.Models;
+
+namespace MyWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetByID(string id)
+        {
+            try
+            {
+                //LINQ [Object] Query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hangHoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            var hangHoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia
+            };
+            hangHoas.Add(hangHoa);
+            //return CreatedAtAction(nameof(GetAll), new { id = hangHoa.MaHangHoa }, hangHoa);
+            return Ok(new
+            {
+                Success = true,
+                Data = hangHoas
+            });
+        }
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, HangHoa hangHoaEdit)
+        {
+            try
+            {
+                //LINQ [Object] Query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                if (id != hangHoa.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+
+                //update
+                hangHoa.TenHangHoa = hangHoaEdit.TenHangHoa;
+                hangHoa.DonGia = hangHoaEdit.DonGia;
+                return Ok(hangHoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            try
+            {
+                //LINQ [Object] Query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                hangHoas.Remove(hangHoa);
+                return Ok(hangHoas);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
+
---
+++ MyWebApi/MyWebApi/Models/HangHoa.cs
@@ -0,0 +1,13 @@
+﻿namespace MyWebApi.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public string DonGia { get; set; }
+    }
+
+    public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
